8815e242dc51e0f8c319e981e86abe4cec429c78
laurenlong <leolauren@163.com>
2023-12-28 14:04:13 +0800

    hw2

---
+++ 22351137龙咏柳/Project2/Project1/moon.h
@@ -0,0 +1,41 @@
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+#include <gl\GL.h>
+
+class Moon
+{
+private:
+	// distance from its planet
+	float distanceFromPlanet;
+
+	// time it takes to complete 1 orbit
+	float orbitTime;
+
+	// time it takes to spin 360 degrees
+	float rotationTime;
+
+	// radius of the moon itself
+	float radius;
+
+	// the texture used for rendering
+	GLuint textureHandle;
+
+	// its position in 3d space relative to the planet
+	float position[3];
+	// its rotation around its axis
+	float rotation;
+
+public:
+	// Distance is in units of km (kilometers) and time is in units of earth days
+	Moon(float distanceFromPlanet, float orbitTime, float rotationTime, float radius, GLuint textureHandle);
+
+	// Calculate its position in 3d space relative to the planet in the orbit using the given time value
+	void calculatePosition(float time);
+
+	// Render it to the screen
+	void render(void);
+
+	// render this moons orbit circle
+	void renderOrbit(void);
+};
