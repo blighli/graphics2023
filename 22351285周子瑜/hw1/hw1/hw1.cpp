e922f4642a4f93e1ca4cf685e68e8e6139e0c366
ziyuchuu <150325912+ziyuchuu@users.noreply.github.com>
2023-12-30 13:22:46 +0800

    更新作业1

---
+++ 22351285周子瑜/hw1/hw1/hw1.cpp
@@ -0,0 +1,37 @@
+﻿#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void MyDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, 300, 300);
+
+    // Set the color to red
+    glColor3f(1.0, 0.0, 0.0);
+
+    glBegin(GL_POLYGON);
+    glVertex3f(-0.5, -0.5, 0.0);
+    glVertex3f(0.5, -0.5, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowSize(300, 300);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("OpenGL Sample Drawing");
+
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+    glutDisplayFunc(MyDisplay);
+    glutMainLoop();
+    return 0;
+}
